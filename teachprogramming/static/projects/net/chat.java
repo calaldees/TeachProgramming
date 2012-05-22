a811ef6dae9e25f4aebd7b982c51b9990dda5ccd
Allan Callaghan <calaldees@hotmail.com>
2012-05-22 16:02:26 +0100

    added java chat client python chat now appends new line character

--- teachprogramming/static/projects/net/chat.java
+++ teachprogramming/static/projects/net/chat.java
@@ -1,50 +1,94 @@
 import java.net.Socket;
 import java.io.*;
-/*
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.OutputStreamWriter;
-import java.io.BufferedWriter;
-*/
-//
 
-public class chat {
+import java.util.Scanner;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class chat implements ActionListener {
+//public class chat {
     
     Socket         network_socket = null;
-    BufferedWriter network_writer = null; //BufferedWriter
-    DataInputStream  network_reader = null; //BufferedReader
+    BufferedWriter network_writer = null;
+    BufferedReader network_reader = null;
     
-    public static void main(String[] args) {new chat("localhost", 9872);}
+    Scanner console = new Scanner(System.in);
     
-    public chat(String host, int port) {
-        connect(host,port);
-        send("HelloWorld");
-    }
-
+    JTextField text_field;
+    JTextArea  text_area;
+    
     public void connect(String host, int port) {
         try {
             network_socket = new Socket(host, port);
-            network_writer = new BufferedWriter(new OutputStreamWriter( network_socket.getOutputStream() )); // new DataOutputStream( 
-            network_reader = new DataInputStream(  network_socket.getInputStream()  ); //new BufferedReader(new InputStreamReader( 
+            network_writer = new BufferedWriter(new OutputStreamWriter( network_socket.getOutputStream() ));
+            network_reader = new BufferedReader(new InputStreamReader(  network_socket.getInputStream()  )); 
             new ReciveThread(this);
         }
         catch (Exception e) {}
     }
     
     public void disconnect() {
-        try {network_writer.flush(); network_writer.close(); network_writer=null;} catch (Exception e) {}//e.printStackTrace();}
-        try {                        network_reader.close(); network_reader=null;} catch (Exception e) {}//e.printStackTrace();}
-        try {                        network_socket.close(); network_socket=null;} catch (Exception e) {}//e.printStackTrace();}
+        try {
+            network_writer.flush();
+            network_writer.close();
+            network_reader.close();
+            network_socket.close();
+        }
+        catch (Exception e) {}//e.printStackTrace();}
     }
     
     public boolean send(String msg) {
-        try                 {network_writer.write(msg); network_writer.flush(); return true; } 
-        catch (Exception e) {                           return false;}
+        try {
+            network_writer.write(msg);
+            network_writer.write("\n");
+            network_writer.flush();
+            return true;
+        } 
+        catch (Exception e) {
+            return false;
+        }
     }
     
     public void receive(String msg) {
-        System.out.println(msg);
+        //System.out.println(msg);
+        text_area.append(msg + "\n");
+        text_area.setCaretPosition(text_area.getDocument().getLength());
     }
+    
+    public void actionPerformed(ActionEvent event) {
+        send(text_field.getText());
+        text_field.setText("");
+    }
+    
+    public chat(String host, int port) {
+        JFrame frame = new JFrame("Chat Client");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        text_field = new JTextField();
+        text_field.addActionListener(this);
+        frame.add(text_field,BorderLayout.SOUTH);
+        
+        text_area = new JTextArea();
+        text_area.setEditable(false);
+        text_area.setLineWrap(true);
+        //frame.add(text_area,BorderLayout.CENTER);
+        JScrollPane scroll = new JScrollPane(text_area, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        frame.add(scroll,BorderLayout.CENTER);
+        
+        frame.pack();
+        frame.setVisible(true);
+        
+        connect(host,port);
+        //send("HelloWorld");
+        while (network_socket.isConnected()) {
+            send(console.nextLine());
+        }
+    }
+
+    public static void main(String[] args) {new chat("localhost", 9872);}
 }
 
 
@@ -60,21 +104,10 @@ class ReciveThread extends Thread {
 
     public void run() {
         while (c.network_socket.isConnected()) {
-            try {
-                //int bytes_avalable = c.network_reader.available();
-                //if (bytes_avalable>0) {
-                //    byte[] b = new byte[bytes_avalable];
-                //    c.network_reader.read(b);
-                //    c.network_reader.skipBytes(bytes_avalable);
-                //    System.out.println(new String(b));
-                //}
-                System.out.println(c.network_reader.read());
-            }
+            try                 {c.receive(c.network_reader.readLine());}
+            catch (Exception e) {c.disconnect();}
+            try                 {sleep(0);}
             catch (Exception e) {}
-            //try                 {c.receive(c.network_reader.readLine());}
-            //catch (Exception e) {c.disconnect();}
-            //try                 {sleep(0);}
-            //catch (Exception e) {}
         }
     }
 }
--- teachprogramming/static/projects/net/chat.py
+++ teachprogramming/static/projects/net/chat.py
@@ -17,7 +17,7 @@ def connection(sock):
             break
         #print(data_recv)
         output_box.insert(tkinter.END, data_recv)
-        output_box.insert(tkinter.END, '\n')
+        #output_box.insert(tkinter.END, '\n')
         output_box.yview(tkinter.END)
     sock.close()
 
@@ -28,6 +28,7 @@ thread.daemon=True
 thread.start()
 
 root = tkinter.Tk()
+root.title("Chat Client")
 
 output_box = tkinter.scrolledtext.ScrolledText(root, width=40, height=15)
 output_box.pack(fill=tkinter.BOTH, expand=1)
@@ -35,7 +36,7 @@ output_box.pack(fill=tkinter.BOTH, expand=1)
 input_box = tkinter.Entry(root)
 input_box.pack(fill=tkinter.BOTH)
 def handle_user_input(e):
-    sock.sendall(input_box.get().encode('utf-8'))
+    sock.sendall((input_box.get()+"\n").encode('utf-8'))
     input_box.delete(0, tkinter.END)
 input_box.bind("<KeyRelease-Return>", handle_user_input)
 input_box.focus_set()
@@ -44,7 +45,7 @@ root.mainloop()
 
 #try:
 #    while True:
-#        sock.sendall(raw_input())
+#        sock.sendall(raw_input()+"\n")
 #finally:
 #    sock.close()
 #    running = False
