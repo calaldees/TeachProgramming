3323a50581db67441be59ca226b5901c6ef9868f
Allan Callaghan <calaldees@hotmail.com>
2013-05-21 18:31:03 +0100

    smooth scroll on click of ancor links

--- teachprogramming/static/site/main.js
+++ teachprogramming/static/site/main.js
@@ -7,3 +7,23 @@ function apply_active_anchor($a) {
         $a.attr('href', $a.attr('href')+$(this).attr('href'));
     });
 }
+
+function scrollTo ($el) {
+    $('html, body').animate({
+        scrollTop: $el.offset().top
+    }, 500);
+} 
+
+// Init
+$(document).ready(function() {
+    // Hijack any links that are clicked
+    // if they start with '#' then scroll to them smoothly and prevent the following of the actual link-page change
+	$('a[href^="#"]').on('click', function(){
+        var
+        href  = $(this).prop('href'),
+        $hash = $(href.substr(href.indexOf('#')));
+        console.log($hash);
+        scrollTo($hash);
+        return false;
+    });
+});
