20ce058b39cda1960c5f16790a7f01b8c72cb0ea
Allan <allan.callaghan@thisisglobal.com>
2014-10-07 08:05:51 +0100

    Java stub

---
+++ teachprogramming/static/language_reference/Java.java
@@ -0,0 +1,15 @@
+public class Java {
+    
+    void hello_world() {
+        //public class HelloWorld {                                             // VER: hello_world
+            //public static void main(String[] args) {                          // VER: hello_world
+                System.out.println("Hello World");                              // VER: hello_world
+            //}                                                                 // VER: hello_world
+        //}                                                                     // VER: hello_world
+    }
+    
+    public static void main(String[] args) {
+        Java j = new Java();
+        j.hello_world();
+    }
+}
