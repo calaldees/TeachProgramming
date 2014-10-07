07c7e3bd7c7fb737cb1e02ef656c096017e4522f
Allan <allan.callaghan@thisisglobal.com>
2014-10-07 07:57:54 +0100

    Added PHP langauge_reference stub

---
+++ teachprogramming/static/language_reference/php.php
@@ -0,0 +1,15 @@
+<?php
+# PHP 5                                        # VER: title
+# windows.php.net/download/                    # VER: download
+# www.apachefriends.org/en/xampp.html          # VER: download
+# XAMPP Lite (if you want a mini web server)   # VER: download
+
+function hello_world() {
+#<?php                    # VER: hello_world
+  echo "Hello World\n";   # VER: hello_world
+# >                       # VER: hello_world
+}
+
+hello_world()
+
+?>
