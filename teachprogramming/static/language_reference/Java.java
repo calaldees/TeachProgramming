8878c15dca5405ab1b24f5604de4e342a596f450
Allan <allan.callaghan@thisisglobal.com>
2014-10-09 08:12:23 +0100

    Started Java snippets

--- teachprogramming/static/language_reference/Java.java
+++ teachprogramming/static/language_reference/Java.java
@@ -1,3 +1,9 @@
+import java.util.Scanner; //Add at top of file                                  // VER: read_line_from_console
+
+import java.io.BufferedReader; //Add at top of file                             // VER: read_file
+import java.io.FileReader;                                                      // VER: read_file
+
+
 public class Java {
     /*
     Java                                                                        // VER: title
@@ -5,6 +11,8 @@ public class Java {
     Java SE (Standard Edition) JDK (Java Development Kit)                       // VER: download
     http://java.sun.com/docs/books/tutorial/                                    // VER: help
     */
+
+    Scanner console = new Scanner(System.in);                                   // VER: read_line_from_console
     
     void hello_world() {
         //public class HelloWorld {                                             // VER: hello_world
@@ -14,8 +22,29 @@ public class Java {
         //}                                                                     // VER: hello_world
     }
     
+    void read_line_from_console() {
+        String username = console.nextLine();                                   // VER: read_line_from_console
+    }
+
+    void read_file() {
+        try {                                                                   // VER: read_file
+            int line_count = 0;                                                 // VER: read_file
+            String line;                                                        // VER: read_file
+            BufferedReader input =  new BufferedReader(new FileReader("in.txt"));// VER: read_file
+            while (( line = input.readLine()) != null) {                        // VER: read_file
+                System.out.println("Line " + line_count + ": " + line);         // VER: read_file
+                line_count += 1;                                                // VER: read_file
+            }                                                                   // VER: read_file
+            input.close();                                                      // VER: read_file
+        }                                                                       // VER: read_file
+        catch (Exception e) {                                                   // VER: read_file
+            System.err.println("Error: " + e.getMessage());                     // VER: read_file
+        }                                                                       // VER: read_file
+    }
+    
     public static void main(String[] args) {
         Java j = new Java();
         j.hello_world();
+        j.read_file();
     }
 }
