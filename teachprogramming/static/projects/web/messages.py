d27905d812ab687e21d86e15b93c14156e4965d5
Allan Callaghan <calaldees@gmail.com>
2017-03-30 07:44:13 +0100

    Added version names to messages.py

--- teachprogramming/static/projects/web/messages.py
+++ teachprogramming/static/projects/web/messages.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python
 
-# ln -s ./ cgi-bin
-# chmod 755 /cgi-bin/messages.py
-# chmod 755 /cgi-bin/web_cgi.py
-# python -m CGIHTTPServer 8000
-# python -m webbrowser -t "http://localhost:8000/cgi-bin/messages.py"
-
 from web_cgi import Webpage, env
 w = Webpage()
 
-if 'logout' in w.params:
-    del w.cookie['username']
-if 'username' in w.params:
-    w.cookie['username'] = w.params['username']
-    w.cookie['username']['max-age'] = 3600
-
+if 'logout' in w.params:                            # VER: logout
+    del w.cookie['username']                        # VER: logout
+if 'username' in w.params:                          # VER: login
+    w.cookie['username'] = w.params['username']     # VER: login
+    w.cookie['username']['max-age'] = 3600          # VER: login
+                                                    # VER: login
 w.print_headers()
 
 print("<html><head>")
@@ -23,38 +17,39 @@ print("</head><body>")
 print("")
 print("<h1>Message Board</h1>")
 print("")
-
-if 'username' not in w.cookie:
-    print("<form action='' method='post'>")
-    print("  Username:<input type='text' name='username' />")
-    print("  <input type='submit' value='Login' />")
-    print("</form>")
-else:
-    print("<p>you are logged in as %s" % w.cookie['username'].value)
-    print("<form action='' method='post'>")
-    print("  Message:<input type='textarea' name='message'       />")
-    print("  Logout: <input type='checkbox' name='logout'        />")
-    print("          <input type='submit'   value='Post Message' />")
-    print("</form>")
-
-    messageboard_filename = "messages.txt"
-
-    # If there is a message then append it to the end of the file
-    if 'message' in w.params:
-        file = open(messageboard_filename, 'a')
-        file.write("<p>%s: %s</p>\n" % (w.cookie['username'].value, w.params['message']))
-        file.close()
-
-    try:
-        # Open the file in read mode
-        file = open(messageboard_filename, 'r')
-        text = file.read()
-        file.close()
-
-        # Print the text file
-        print(text)
-    except:
-        pass
+                                                                        # VER: login
+if True:                                                                # VER: textarea NOT login
+if 'username' not in w.cookie:                                          # VER: login
+    print("<form action='' method='post'>")                             # VER: login
+    print("  Username:<input type='text' name='username' />")           # VER: login
+    print("  <input type='submit' value='Login' />")                    # VER: login
+    print("</form>")                                                    # VER: login
+else:                                                                   # VER: login
+    print("<p>you are logged in as %s" % w.cookie['username'].value)    # VER: login
+    print("<form action='' method='post'>")                             # VER: textarea
+    #print("  Username:<input type='text' name='username'  />")    # VER: username NOT login
+    print("  Message:<input type='textarea' name='message'/>")   # VER: textarea
+    print("  Logout: <input type='checkbox' name='logout' />")   # VER: logout
+    print("  <input type='submit'   value='Post Message'  />")   # VER: textarea
+    print("</form>")                                                    # VER: textarea
+                                                                                              # VER: save_message
+    messageboard_filename = "messages.txt"                                                    # VER: save_message
+                                                                                              # VER: save_message
+    ## If there is a message then append it to the end of the file                             # VER: save_message
+    if 'message' in w.params:                                                                 # VER: save_message
+        file = open(messageboard_filename, 'a')                                               # VER: save_message
+        #file.write("<p>%s</p>\n" % (w.params['message']))                                    # VER: save_message NOT username
+        #file.write("<p>%s: %s</p>\n" % (w.params['username'].value, w.params['message']))    # VER: username     NOT login
+        file.write("<p>%s: %s</p>\n" % (w.cookie['username'].value, w.params['message']))     # VER: login
+        file.close()                                                                          # VER: save_message
+                                                      # VER: show_messages
+    try:                                              # VER: show_messages
+        file = open(messageboard_filename, 'r')       # VER: show_messages
+        text = file.read()                            # VER: show_messages
+        file.close()                                  # VER: show_messages
+        print(text)                                   # VER: show_messages
+    except:                                           # VER: show_messages
+        pass                                          # VER: show_messages
 
 print("")
 print("</body></html>")
