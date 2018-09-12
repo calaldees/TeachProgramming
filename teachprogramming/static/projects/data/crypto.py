35585bb09b04e4f65442e11f66c06dc316c493eb
Allan Callaghan <calaldees@gmail.com>
2018-09-12 12:52:38 +0100

    Added ideas for future exercises

---
+++ teachprogramming/static/projects/data/crypto.py
@@ -0,0 +1,8 @@
+"""
+placeholder for 'passphrase' cypher exercise
+https://en.wikipedia.org/wiki/Frequency_analysis
+
+Given a passphrase can a message be encrypted/decrypted
+
+This is an exercise for string manipulation and ascii
+"""
---
+++ teachprogramming/static/projects/data/graphs.py
@@ -0,0 +1,4 @@
+"""
+Given a dataset, can a graph be drawn
+Maybe use PIL? maybe use a graph library as two examples
+"""
