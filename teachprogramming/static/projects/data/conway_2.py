496dd375ced856dbbd44fd8215197fbe1b3ce729
Allan Callaghan <calaldees@gmail.com>
2019-02-09 15:42:48 +0000

    Added 'set' based python conway_game_of_life Added pygame viewer

---
+++ teachprogramming/static/projects/data/conway_2.py
@@ -0,0 +1,134 @@
+
+class Conway():
+
+    def __init__(self, state=None, width=0, height=0):
+        self._state = state or set()
+        self.width = width
+        self.height = height
+
+    def get_cell(self, x, y):
+        return (x, y) in self._state
+
+    def next(self):
+        def _count_neighbours(i):
+            x, y = i
+            return len(self._state & {
+                (x + _x, y + _y)
+                for _y in (-1, 0, 1)
+                for _x in (-1, 0, 1)
+                if not (_x == 0 and _y == 0)
+            })
+        def _is_cell_alive(i):
+            neighbour_count = _count_neighbours(i)
+            return (
+                neighbour_count == 3 or
+                (i in self._state and neighbour_count == 2)
+            )
+        def _cells_to_check_generator():
+            for i in self._state:
+                x, y = i
+                yield from {
+                    (x + _x, y + _y)
+                    for _y in (-1, 0, 1)
+                    for _x in (-1, 0, 1)
+                }
+        self._state = {i for i in set(_cells_to_check_generator()) if _is_cell_alive(i)}
+
+
+class ConwayFactory():
+
+    @staticmethod
+    def conway_from_file(filename):
+        return Conway(*ConwayFactory.parse(filename))
+
+    @staticmethod
+    def parse(filename):
+        with open(filename, 'rt', encoding='utf-8') as filehandle:
+            if filename.endswith('.txt'):
+                return ConwayFactory._parse_txt(filehandle)
+            if filename.endswith('.rle'):
+                return ConwayFactory._parse_rle(filehandle)
+
+    @staticmethod
+    def _parse_txt(filehandle):
+        r"""
+        >>> ConwayFactory._parse_txt((
+        ...     '.#.',
+        ...     '###',
+        ...     '..#',
+        ... ))
+        ({(0, 1), (2, 1), (2, 2), (1, 0), (1, 1)}, 3, 3)
+        """
+        state = set()
+        width = 0
+        height = 0
+        for y, line in enumerate(filehandle):
+            height = y + 1
+            width = max(width, len(line))
+            for x, i in enumerate(line.rstrip()):
+                if not (i == '.' or i == ' '):
+                    state.add((x, y))
+        return (state, width, height)
+
+    @staticmethod
+    def _parse_rle(filehandle):
+        r"""
+        http://www.conwaylife.com/wiki/RLE
+
+        >>> ConwayFactory._parse_rle((
+        ...     '#C This is a glider.',
+        ...     'x = 3, y = 3',
+        ...     'bo$2bo$3o!',
+        ... ))
+        ({(1, 2), (2, 1), (2, 2), (1, 0), (0, 2)}, 3, 3)
+        """
+        import re
+        state = set()
+        width = 0
+        height = 0
+        x = 0
+        y = 0
+        for line in filehandle:
+            if line.startswith('#'):
+                continue
+            if not width and not height:
+                width, height = map(int, re.search('x\s*=\s*(?P<x>\d+),\s*y\s*=\s*(?P<y>\d+)', line).groups())
+                continue
+            for match in re.finditer(r'(?P<repeat>\d{0,4})(?P<char>[bo$!])', line):
+                repeats, cell = match.groups()
+                repeats = int(repeats) if repeats else 1
+                if cell == 'o':
+                    for i in range(repeats):
+                        state.add((x, y))
+                        x += 1
+                if cell == 'b':
+                    x += repeats
+                if cell == '$':
+                    for i in range(repeats):
+                        y += 1
+                    x = 0
+                if cell == '!':
+                    break
+            y += 1
+            x = 0
+        return (state, width, height)
+
+
+def conway_display(conway):
+    for y in range(conway.height):
+        print(''.join(
+            '#' if conway.get_cell(x, y) else '.'
+            for x in range(conway.width)
+        ))
+
+
+if __name__ == "__main__":
+    conway = ConwayFactory.conway_from_file('conway.txt')
+    conway_display(conway)
+
+    import time
+    for i in range(20):
+        time.sleep(1)
+        print('')
+        conway.next()
+        conway_display(conway)
---
+++ teachprogramming/static/projects/data/conway_pygame.py
@@ -0,0 +1,58 @@
+import pygame
+
+import sys
+filename = sys.argv[1] if len(sys.argv) == 2 else 'conway.txt'
+
+from conway_2 import ConwayFactory
+conway = ConwayFactory.conway_from_file(filename)
+
+
+class Variables():
+    running = True
+    paused = True
+    color_background = (0, 0, 0)
+    color_cell_alive = (255, 255, 255)
+    view_x = 0
+    view_y = 0
+v = Variables()
+
+
+if __name__ == "__main__":
+    pygame.init()
+    RESOLUTION = (640, 480)
+    screen = pygame.display.set_mode(RESOLUTION)
+    pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN, 16)
+    clock  = pygame.time.Clock()
+
+
+    while v.running:
+        clock.tick(60)
+        screen.fill(v.color_background)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                v.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    v.paused = not v.paused
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            v.running = False
+
+        MOVE = 4
+        if keys[pygame.K_UP   ]: v.view_y += -MOVE
+        if keys[pygame.K_DOWN ]: v.view_y +=  MOVE
+        if keys[pygame.K_LEFT ]: v.view_x += -MOVE
+        if keys[pygame.K_RIGHT]: v.view_x +=  MOVE
+
+        for i in conway._state:
+            x, y = i
+            screen.set_at((x-v.view_x, y-v.view_y), v.color_cell_alive)
+
+        pygame.display.flip()
+
+        if not v.paused:
+            conway.next()
+
+    pygame.quit()
