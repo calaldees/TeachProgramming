82242dc8645cd9b259d1cbe5af133e1c522331e5
Allan Callaghan <calaldees@gmail.com>
2019-05-25 16:42:49 +0100

    Python plot example

---
+++ teachprogramming/static/projects/plot.py
@@ -0,0 +1,21 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Create random data with numpy
+import numpy as np
+
+N = 1000
+random_x = np.random.randn(N)
+random_y = np.random.randn(N)
+
+# Create a trace
+trace = go.Scatter(
+    x = random_x,
+    y = random_y,
+    mode = 'markers'
+)
+
+data = [trace]
+
+import plotly
+plotly.offline.plot(data, filename='basic-line')
