10224a301972f06b82b60c5f607757e1061b70e6
Allan Callaghan <calaldees@gmail.com>
2020-10-10 10:37:06 +0100

    Added bubbelsort

--- teachprogramming/static/language_reference/languages/csharp/hello.cs
+++ teachprogramming/static/language_reference/languages/csharp/hello.cs
@@ -1,3 +1,6 @@
+// bash shortcut
+// function cs { mcs "$1" && mono "${1%.*}.exe" && rm "${1%.*}.exe"; }
+
 using System;
 
 public class HelloWorld
---
+++ teachprogramming/static/projects/data/bubblesort.py
@@ -0,0 +1,26 @@
+
+def bubbelsort(data):
+    """
+    >>> bubbelsort(['a', 'b', 'c', 'd'])
+    ['a', 'b', 'c', 'd']
+    >>> bubbelsort(['b', 'd', 'c', 'a'])
+    ['a', 'b', 'c', 'd']
+    """
+    items_have_changed = True
+    while items_have_changed:
+        items_have_changed = False
+        #print(data)
+        for i in range(len(data)-1):
+            a = data[i]
+            b = data[i+1]
+            #print(f'comparing {i}:{a} with {i+1}:{b}')
+            if a > b:
+                #print(f'swapping {i}:{a} with {i+1}:{b} -> {i}:{b} and {i+1}:{a}')
+                data[i] = b
+                data[i+1] = a
+                items_have_changed = True
+    #print('no items have changed places - the list is now sorted')
+    return data
+
+if __name__ == '__main__':
+    print(bubbelsort(['b', 'd', 'c', 'a']))
