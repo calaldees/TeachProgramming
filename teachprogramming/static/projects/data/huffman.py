abeb9271d8d3e90bf33376408b52c2e1dca3f60b
Allan Callaghan <calaldees@gmail.com>
2020-10-25 01:54:17 +0100

    Added to huffman with tree crawling to get depths Now trying to encode real byte data but uncovering more errors

--- teachprogramming/static/projects/data/huffman.py
+++ teachprogramming/static/projects/data/huffman.py
@@ -10,11 +10,26 @@ from collections import defaultdict
 from queue import PriorityQueue
 
 class Tree():
-    def __init__(self, i, left=None, right=None):
-        self.i = i
+    def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
-
+    def depth_of_nodes(self):
+        """
+        >>> t = Tree(left='C', right=Tree(left=Tree(left='D', right='A'), right='B'))
+        >>> tuple(t.depth_of_nodes())
+        ((1, 'C'), (2, 'B'), (3, 'D'), (3, 'A'))
+        """
+        stack = [(1, self)]
+        while stack:
+            depth, i = stack.pop()
+            if isinstance(i.left, self.__class__):
+                stack.append((depth+1, i.left))
+            else:
+                yield (depth, i.left)
+            if isinstance(i.right, self.__class__):
+                stack.append((depth+1, i.right))
+            else:
+                yield (depth, i.right)
 
 def _frequency_analysis(data: bytes) -> dict[str, int]:
     """
@@ -62,8 +77,24 @@ def _build_tree(data):
     q = _convert_to_PriorityQueue(_frequency_analysis(data))
     while q.qsize() > 1:
         a, b = q.get(), q.get()
-        q.put(WeightTreeItem(a.weight + b.weight, Tree(None, a.item, b.item)))
+        q.put(WeightTreeItem(a.weight + b.weight, Tree(a.item, b.item)))
     return q.get()
+
+def _normalise_tree_depths(tree):
+    """
+    #>>> t = Tree(left=3, right=Tree(left=Tree(left=4, right=1), right=2))
+    >>> t = _build_tree(bytes(range(256)))
+    >>> _normalise_tree_depths(t)
+    """
+    i_depth = sorted((i, depth) for depth, i in sorted(tree.depth_of_nodes()))
+    def pop_if_match(i):
+        if i_depth:
+            _i, _depth = i_depth[0]
+            if i == _i:
+                i_depth.pop(0)
+                return _depth
+        return 0
+    return tuple(pop_if_match(i) for i in range(256))
 
 
 def encode(data: bytes) -> bytes:
