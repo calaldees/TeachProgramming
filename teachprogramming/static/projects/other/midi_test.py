b5c17970f72f74c0f6a86d0f52dbb307997fb5c4
Allan Callaghan <calaldees@gmail.com>
2022-01-18 15:07:35 +0000

    More comments to describe midi_dmx controler

--- teachprogramming/static/projects/other/midi_test.py
+++ teachprogramming/static/projects/other/midi_test.py
@@ -23,10 +23,10 @@ midi_in.open_port(1)
 def handle_input(event, data=None):
     message, deltatime = event
     #print(f'message: {message}, deltatime: {deltatime}, data: {data}')
-    i, v, _ = message
-    i -= 224
-    v *= 2
-    if i<0 or i>=8:
+    i, v, _ = message  # input (slider number), value
+    i -= 224  # for the nanoKONTROL I was using, the first slider is numbered 224 (probably encoding cc-channel and other stuff in this, but the sliders are sequential so subtracting a constant is enough)
+    v << 1  #v *= 2  # midi values are 7bit 0-127 we need to expand this to 0-255 8bit
+    if i<0 or i>=8:  # only use the first 8 sliders of the nanoKONTROL - anything outside this ignore
         return
     print(f'{i}:{v}')
     #breakpoint()
