dc65e261db0b1a32c1a0ce568c8b5571f15e1075
Allan Callaghan <calaldees@gmail.com>
2022-01-18 15:06:28 +0000

    Tidyed example api implementation

--- teachprogramming/static/projects/web/rest_api/falcon_api.py
+++ teachprogramming/static/projects/web/rest_api/falcon_api.py
@@ -1,5 +1,4 @@
 import logging
-import random
 import datetime
 
 import falcon
@@ -7,13 +6,13 @@ import falcon
 log = logging.getLogger(__name__)
 
 ITEMS = {
-    1: {
+    1: {  # have test data in system on startup
         'id': 1,
         'user_id': "user1234",
         'keywords': ["hammer", "nails", "tools"],
         "description": "A hammer and nails set",
-        "lat": (random.random() * (70*2)) - 70,
-        "lon": (random.random() * (180*2)) - 180,
+        "lat": 51.2798438, 
+        "lon": 1.0830275,
         "date_from": datetime.datetime.now().isoformat(),
     }
 }
@@ -31,16 +30,7 @@ class IndexResource:
         resp.status = falcon.HTTP_204
     def on_get(self, req, resp):
         resp.content_type = falcon.MEDIA_HTML
-        resp.text = """
-<html>
-<head>
-<title>Test</title>
-</head>
-<body>
-<h1>Test</h1>
-</body>
-</html>
-"""
+        resp.text = """<html><head><title>Test</title></head><body><h1>Test</h1></body></html>"""
 
 class ItemsResource:
     def on_options(self, req, resp):
@@ -51,15 +41,11 @@ class ItemsResource:
             items = tuple(i for i in items if i['user_id'] == req.params.get('user_id'))
         resp.media = items
 
-
 class ItemResource:
-
     def on_options(self, req, resp):
         resp.status = falcon.HTTP_204
-
     def on_head(self, req, resp):
-        pass
-
+        pass  # TODO: cant remember why this is a null implementation - maybe it can be removed?
     def on_post(self, req, resp):
         item_id = max(ITEMS.keys()) + 1
         FIELDS = frozenset(req.media.keys())
@@ -74,51 +60,50 @@ class ItemResource:
         })
         resp.status = falcon.HTTP_201
         resp.media = ITEMS[item_id]
-
-
     def on_get(self, req, resp, item_id):
         item_id = int(item_id)
         try:
             resp.media = ITEMS[item_id]
         except KeyError:
             resp.status = falcon.HTTP_404
-
     def on_delete(self, req, resp, item_id):
         item_id = int(item_id)
         try:
             del ITEMS[item_id]  # TODO: this is terrible because it re-uses ids .. 
-            resp.status = falcon.HTTP_201
+            resp.status = falcon.HTTP_204
         except KeyError:
             resp.status = falcon.HTTP_404
 
-app = falcon.App(middleware=[
-    AddCORSMiddleware(),
-])
 
+def create_wsgi_app(**kwargs):
+    app = falcon.App(middleware=[
+        AddCORSMiddleware(),
+    ])
 
-index_resource = IndexResource()
-app.add_route('/', index_resource)
+    index_resource = IndexResource()
+    app.add_route('/', index_resource)
 
-item_resource = ItemResource()
-app.add_route('/item', item_resource)
-app.add_route('/item/{item_id}', item_resource)
+    item_resource = ItemResource()
+    app.add_route('/item', item_resource)
+    app.add_route('/item/{item_id}', item_resource)
 
-items_resource = ItemsResource()
-app.add_route('/items', items_resource)
+    items_resource = ItemsResource()
+    app.add_route('/items', items_resource)
+
+    return app
 
 
 if __name__ == '__main__':
-    #kwargs = get_args()
-    kwargs = {
-        "host": "0.0.0.0",
-        "port": 8000
-    }
-    logging.basicConfig(level=logging.DEBUG)  #kwargs['log_level']
+    kwargs = {"host": "0.0.0.0", "port": 8000, "log_level": logging.DEBUG}  # TODO: get kwargs from argparse
+    logging.basicConfig(level=kwargs['log_level'])
 
     from wsgiref import simple_server
-    httpd = simple_server.make_server(kwargs['host'], kwargs['port'], app)  #create_wsgi_app(**kwargs)
+    httpd = simple_server.make_server(kwargs['host'], kwargs['port'], create_wsgi_app(**kwargs))
     try:
         log.info('start')
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
+
+
+app = create_wsgi_app()  # surface `app`` variable for gunicorn
