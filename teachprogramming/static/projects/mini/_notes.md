0c4010f81220518266cffb2f7509c3be2f054bcc
Allan Callaghan <calaldees@gmail.com>
2022-02-16 10:21:04 +0000

    Added FizzBuzz Moved notes around

---
+++ teachprogramming/static/projects/mini/_notes.md
@@ -0,0 +1,60 @@
+Unsorted Notes
+==============
+
+
+Bit-manipulation
+String manipulation
+Recursion
+
+String '.''-' morse code
+
+
+Next year - join the dots - Egineering
+
+https://www.educative.io/edpresso/what-is-the-rsa-algorithm
+https://scanftree.com/programs/c/c-code-to-implement-rsa-algorithmencryption-and-decryption/
+https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.rsacryptoserviceprovider?view=net-6.0
+
+
+* [Instructional Pseudocode Guide to Teach Problem-Solving](https://dl.acm.org/doi/10.1145/3304221.3325581) 2019
+    * > This paper introduces how technical writing techniques and problem-solving strategies can be combined to help students find those questions and develop programming code through writing instructions and documenting their design process. 
+
+
+
+
+Fibbonach
+---------
+```python
+def fib(v):
+    """
+    >>> next_fib(0):
+    1
+    >>> next_fib(1):
+    1
+    """
+    return
+```
+
+
+
+
+
+
+is_anagram(a, b) {
+
+}
+
+
+Single digets only
+123 ok
+1223 not ok
+321 ok
+3321 not ok
+67 ok
+66 not ok
+
+
+
+
+is_prime
+
---
+++ teachprogramming/static/projects/mini/fizzbuzz.py
@@ -0,0 +1,18 @@
+def fizzbuzz(i):
+    """
+    * [FizzBuzz](https://en.wikipedia.org/wiki/Fizz_buzz#Programming_interviews) [_](https://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developers-who-grok-coding/)
+        * > Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+        * In under 5min (Pro) - in under 10min (A Grade)
+        * [Tom Scott commentary and solution](https://www.youtube.com/watch?v=QPZ0pIK_wsc)
+
+    >>> tuple(map(fizzbuzz, range(100)))
+    ('fizzbuzz', 1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz', 'fizz', 22, 23, 'fizz', 'buzz', 26, 'fizz', 28, 29, 'fizzbuzz', 31, 32, 'fizz', 34, 'buzz', 'fizz', 37, 38, 'fizz', 'buzz', 41, 'fizz', 43, 44, 'fizzbuzz', 46, 47, 'fizz', 49, 'buzz', 'fizz', 52, 53, 'fizz', 'buzz', 56, 'fizz', 58, 59, 'fizzbuzz', 61, 62, 'fizz', 64, 'buzz', 'fizz', 67, 68, 'fizz', 'buzz', 71, 'fizz', 73, 74, 'fizzbuzz', 76, 77, 'fizz', 79, 'buzz', 'fizz', 82, 83, 'fizz', 'buzz', 86, 'fizz', 88, 89, 'fizzbuzz', 91, 92, 'fizz', 94, 'buzz', 'fizz', 97, 98, 'fizz')
+    """
+    r = ''
+    if i % 3 == 0:
+        r += 'fizz'
+    if i % 5 == 0:
+        r += 'buzz'
+    if not r:
+        r = i
+    return r
