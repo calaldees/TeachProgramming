9dd4901e3afbf1d3cc9c8d05b81e27c6a925382c
Allan Callaghan <calaldees@gmail.com>
2022-02-23 16:30:54 +0000

    max_profit method2 was not complete

--- teachprogramming/static/projects/mini/max_profit.py
+++ teachprogramming/static/projects/mini/max_profit.py
@@ -39,18 +39,36 @@ def maxProfit(prices: List[int]) -> int:
     0
 
     Explanation: In this case, no transactions are done and the max profit = 0.
+
+    --- some of my tests
+
+    >>> maxProfit([6,5,10,1,2,3,4])
+    5
     """
-    return method2(prices)
+    return method1(prices)
+
+
+
+import random, sys
+def gen_random_prices(numer_of_random_prices):
+    return tuple(
+        random.randint(0, sys.maxsize)
+        for i in range(numer_of_random_prices)
+    )
+
+
+def _methods_are_the_same(methods=(method1, method2)):
+    """
+    >>> _methods_are_the_same()
+    """
+    for i in range(100):
+        data = gen_random_prices(2000)
+        assert len(set(method(data) for method in methods)) == 1, f"Methods do not produce the same results"
 
 
 if __name__ == "__main__":
     print("Running timeit benchmark")
-    import timeit, random, sys
-    def gen_random_prices(numer_of_random_prices):
-        return list(
-            random.randint(0, sys.maxsize)
-            for i in range(numer_of_random_prices)
-        )
+    import timeit
     average_time_taken = [
         timeit.timeit(lambda: method(gen_random_prices(2000)), number=3)
         for method in (method1, method2)
