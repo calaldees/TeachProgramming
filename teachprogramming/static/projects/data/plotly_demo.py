0e066110abf1ae4d6230bd811068981c6c7ccca5
Allan Callaghan <calaldees@gmail.com>
2022-03-10 13:22:12 +0000

    Renamed plotly

--- teachprogramming/static/projects/data/plotly_demo.py
+++ teachprogramming/static/projects/data/plotly_demo.py
@@ -7,10 +7,10 @@ import random
 import plotly
 
 N = 1000
-trace = plotly.graph_objs.Scatter(
+scatter = plotly.graph_objs.Scatter(
     x = [random.randint(0,N) for x in range(N)],
     y = [random.randint(0,N) for y in range(N)],
     mode = 'markers'
 )
 
-plotly.offline.plot([trace], filename='basic-scatter.html')
+plotly.offline.plot([scatter], filename='basic-scatter.html', auto_open=False)
