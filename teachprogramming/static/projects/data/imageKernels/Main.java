31d38ec89ad5a48635e63d4953f53d0a6c6021e6
Allan Callaghan <calaldees@gmail.com>
2023-02-14 12:05:45 +0000

    Added threading of kernel operations

--- teachprogramming/static/projects/data/imageKernels/Main.java
+++ teachprogramming/static/projects/data/imageKernels/Main.java
@@ -14,11 +14,14 @@ class Main {
         final Random r = new Random();
         Supplier<Integer> rand_int = () -> r.nextInt(0,10);
 
-        Matrix m1 = new Matrix(new Dimension(10,10), rand_int);
+        Matrix m1 = new Matrix(new Dimension(40,40), rand_int);
         System.out.println(m1.toString());
 
         Matrix m2 = MatrixOperation.apply(m1, Kernel.BLUR);
         System.out.println(m2.toString());
+
+        Matrix m3 = MatrixOperation.applyThreaded(m1, Kernel.BLUR);
+        System.out.println(m3.toString());
 
         //System.out.println(mm.m.length);
     }
--- teachprogramming/static/projects/data/imageKernels/imageKernels/MatrixOperation.java
+++ teachprogramming/static/projects/data/imageKernels/imageKernels/MatrixOperation.java
@@ -1,17 +1,49 @@
 package imageKernels;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+
 import imageKernels.Matrix;
 import imageKernels.Kernel;
 
-public class MatrixOperation {
 
-    public static Matrix apply(Matrix m, Kernel k) {
-        Matrix m2 = m.clone();
-        for (int i=0 ; i<m.m.length ; i++) {
-            m2.m[i] = k.apply(m.data(i));
-        }
+public class MatrixOperation {
+    private MatrixOperation() {}
+
+    public static Matrix apply(Matrix m1, Kernel k) {
+        Matrix m2 = m1.clone();
+        apply(m1, k, m2, IntStream.range(0, m1.m.length));
         return m2;
     }
 
-    private MatrixOperation() {}
+    private static void apply(Matrix m1, Kernel k, Matrix m2, IntStream range) {
+        range.forEachOrdered(i -> {
+            //System.out.println(i);
+            m2.m[i] = k.apply(m1.data(i));
+        });
+    }
+
+    public static Matrix applyThreaded(Matrix m1, Kernel k) {return applyThreaded(m1,k,4);}
+    public static Matrix applyThreaded(Matrix m1, Kernel k, int t) {
+        Matrix m2 = m1.clone();
+        IntStream.range(0, t)
+            .mapToObj(threadNumber -> {
+                int batch_size = (int)(m1.m.length / t);
+                int i = threadNumber * batch_size;
+                int j = i + batch_size;
+                String threadName = String.format("%s: %s->%s",threadNumber,i,j);
+                System.out.println(threadName);
+                Thread thread = new Thread(() -> apply(m1, k, m2, IntStream.range(i,j)),  threadName);
+                thread.start();
+                return thread;
+            })
+            .forEach(thread -> {
+                try {thread.join();}
+                catch (InterruptedException ex) {}
+            });
+        return m2;
+    }
 }
