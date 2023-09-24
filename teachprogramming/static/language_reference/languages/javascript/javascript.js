5332704fdd34f6e1b747f210a620478076342a25
Allan Callaghan <allan@calaldees.uk>
2023-09-24 14:40:32 +0100

    Reduced col width of JS on lanagueg cheat sheet

--- teachprogramming/static/language_reference/languages/javascript/javascript.js
+++ teachprogramming/static/language_reference/languages/javascript/javascript.js
@@ -318,12 +318,13 @@ function _sleep() {
   let timeout = setTimeout(after_sleep, 1000);    // VER: sleep
 
 
-  const sleep = (milliseconds) => new Promise((resolve, reject) => setTimeout(resolve, milliseconds));  // VER: sleep
+  const sleep = (milliseconds) => new Promise(                // VER: sleep
+    (resolve, reject) => setTimeout(resolve, milliseconds));  // VER: sleep
   async function do_steps() {// VER: sleep
       console.log("1"); // VER: sleep
       await sleep(1000); // VER: sleep
       console.log("2"); // VER: sleep
-  }// VER: sleep
+  } // VER: sleep
 }
 
 
