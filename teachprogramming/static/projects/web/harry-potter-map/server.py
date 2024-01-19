96f78329b1109a7307dd8590137e2c1219d2a2ff
Allan Callaghan <allan@calaldees.uk>
2024-01-19 22:13:05 +0000

    Skeleton of the harry potter map POC structure working 100 lines of py/html/js code. Amazing!

--- teachprogramming/static/projects/web/harry-potter-map/server.py
+++ teachprogramming/static/projects/web/harry-potter-map/server.py
@@ -1,42 +1,45 @@
-from aiohttp import web, WSCloseCode
+import asyncio
+import json
+
+from aiohttp import web, WSCloseCode, WSMsgType
 
 websockets = set()
+data = {
+    'harry': {'lat':0, 'lon':0},
+}
 
-# https://docs.aiohttp.org/en/stable/#server-example
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
-
-# https://docs.aiohttp.org/en/stable/web_quickstart.html#websockets
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
     websockets.add(ws)
     print('websocket connection open')
     async for msg in ws:
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            if msg.data == 'close':
-                await ws.close()
-            else:
-                await ws.send_str(msg.data + '/answer')
-        elif msg.type == aiohttp.WSMsgType.ERROR:
+        if msg.type == WSMsgType.TEXT:
+            msg_data = json.loads(msg.data)
+            data[msg_data.get('name', 'Unknown')] = msg_data
+        elif msg.type == WSMsgType.ERROR:
             print('ws connection closed with exception %s' % ws.exception())
     print('websocket connection closed')
     websockets.remove(ws)
     return ws
 
 
-async def send_websocket_timed_state(app):
+async def send_websocket_timed_state():
     print("send_websocket_timed_state")
-    return
+    while True:
+        #print(f'WebSockets: {len(websockets)}')
+        for ws in tuple(websockets):
+            await ws.send_str(json.dumps(data))
+        await asyncio.sleep(1)
 
+async def on_startup(app):
+    print('on_startup')
+    asyncio.create_task(send_websocket_timed_state())
 async def on_shutdown(app):
-    print('shutdown')
+    print('on_shutdown')
     for ws in tuple(websockets):
         await ws.close(code=WSCloseCode.GOING_AWAY, message='shutdown')
 
-# https://stackoverflow.com/a/43009754/3356840
 async def index(request):
     return web.FileResponse('./index.html')
 
@@ -44,9 +47,8 @@ app = web.Application()
 app.add_routes([
         web.get('/', index),
         web.get('/ws', websocket_handler),
-        web.get('/{name}', handle),
 ])
-app.on_startup.append(send_websocket_timed_state)
+app.on_startup.append(on_startup)
 app.on_shutdown.append(on_shutdown)
 
 if __name__ == '__main__':
