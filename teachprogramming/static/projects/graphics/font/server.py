b522a3d662b404c04b3f8e73a372ccf341293885
Allan Callaghan <allan@calaldees.uk>
2024-04-04 15:23:55 +0100

    Added font validation to frontend (What a mess .. amature hour)

--- teachprogramming/static/projects/graphics/font/server.py
+++ teachprogramming/static/projects/graphics/font/server.py
@@ -21,7 +21,7 @@ def hex_to_image_data(hex_str):
     >>> hex_to_image_data('ffffffffffffffa5')[:8]
     (True, False, True, False, False, True, False, True)
     """
-    assert len(hex_str) == 16, f'hex string must be 16 chars to complete a 8x8 mono image - got {len(hex_str)}'
+    assert len(hex_str) == 16, f'hex string must be 16 chars to complete a 8x8 mono image - got {len(hex_str)}:{hex_str}'
     n = int(hex_str, 16)
     return tuple(bool((n >> i) & 1) for i in range(64))
 
@@ -47,6 +47,27 @@ app.add_routes([
         web.static('/static', './static'),
 ])
 
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     web.run_app(app, port=8000)
+
+
+
+# TODO: Future error handling?
+# https://docs.aiohttp.org/en/stable/web_advanced.html#example
+# @web.middleware
+# async def error_middleware(request, handler):
+#     try:
+#         response = await handler(request)
+#         if response.status != 404:
+#             return response
+#         message = response.message
+#     except web.HTTPException as ex:
+#         if ex.status != 404:
+#             raise
+#         message = ex.reason
+#     return web.json_response({'error': message})
+# app.middlewares.append(error_middleware)
+
