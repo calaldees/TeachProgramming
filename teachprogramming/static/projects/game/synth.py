8954953bab70047436e0152cec44f81340aa340a
Allan Callaghan <allan@calaldees.uk>
2024-04-23 10:19:19 +0100

    Added noise ocillator renamed sample to frame (deep breath, maybe interpolation next?)

--- teachprogramming/static/projects/game/synth.py
+++ teachprogramming/static/projects/game/synth.py
@@ -1,4 +1,5 @@
 import math
+import random
 from functools import partial
 
 
@@ -8,20 +9,22 @@ def oscillator_generator_8bit_unsigned(func, sample_frequency_hz, note_frequency
 
 OSCILLATOR_SAMPLES = {
     name: oscillator_generator_8bit_unsigned(func, 22050)
+    # functions that take 0.0 to 1.0 input of progress though osculation and return -1.0 to +1.0 of wave
     for name, func in {
         "sine": lambda x: math.sin(x * math.pi * 2),
         "square": lambda x: 1.0 if x < 0.5 else -1.0,
         "sawtooth": lambda x: (((x+0.5) * 2) % 2) - 1,
         "triangle": lambda x: -(abs((((x+0.25)%1)*4) - 2) - 1),
+        "noise": lambda x: random.random()*2-1,
     }.items()
 }
 
-def get_sample(sample, index, rate=1.0):
+def get_frame(sample, index, rate=1.0):
     # TODO interpolation (expand and shrink)
     return sample[index % len(sample)]
 
-def get_samples(sample_index, size, sample):
-    return bytes(map(partial(get_sample, sample), range(sample_index, sample_index+size)))
+def get_sample_bytes(sample_index, size, sample):
+    return bytes(map(partial(get_frame, sample), range(sample_index, sample_index+size)))
 
 import pyaudio
 class Audio():
@@ -30,7 +33,7 @@ class Audio():
         self.audio_frame = 0
         self.audio_stream = self.pyaudio.open(format=pyaudio.paUInt8, channels=1, rate=22050, output=True, stream_callback=self.pyaudio_stream_callback)
     def pyaudio_stream_callback(self, in_data, frame_count, time_info, status):
-        audio_bytes = get_samples(self.audio_frame, frame_count, OSCILLATOR_SAMPLES['sine'])
+        audio_bytes = get_sample_bytes(self.audio_frame, frame_count, OSCILLATOR_SAMPLES['sine'])
         self.audio_frame += frame_count
         return (audio_bytes, pyaudio.paContinue)
     def quit(self):
