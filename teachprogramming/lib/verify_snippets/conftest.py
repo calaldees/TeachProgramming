25a4500e276ee7f224322512845e27dabed89d20
Allan Callaghan <allan@calaldees.uk>
2025-03-10 11:57:55 +0000

    verify_snippets now compiles python code! How many of my activities are not syntactically correct! .. DAMN!

--- teachprogramming/lib/verify_snippets/conftest.py
+++ teachprogramming/lib/verify_snippets/conftest.py
@@ -2,19 +2,112 @@
 # https://docs.pytest.org/en/stable/example/nonpython.html#yaml-plugin
 
 import json
-import tempfile
 from pathlib import Path
-from typing import TypedDict, Unpack
+from typing import NamedTuple, Callable
+from types import MappingProxyType
+import subprocess
+from os import environ
 
 import pytest
 
+
 PATH_PROJECTS = Path("projects").absolute()
+PATH_LANGUAGES = Path('languages').absolute()
+LOOKUP_LANGUAGE_FOLDER = MappingProxyType({
+    'py': 'python'
+})
 
 
-LANGUAGES_SUPPORTED = frozenset({"py"})
+def get_built_docker_language_runners() -> set[str]:
+    result = subprocess.run(
+        ("docker", "images",  "--format", "json"), #"language_runner",
+        capture_output=True,
+        timeout=5,
+        text=True,
+        check=True,
+    )
+    return {container['Tag'] for container in map(json.loads, filter(None, result.stdout.split('\n')))}
+
+def build_docker_language_runner(language) -> subprocess.CompletedProcess:
+    return subprocess.run(
+        ("docker", "build", PATH_LANGUAGES.joinpath(LOOKUP_LANGUAGE_FOLDER.get(language, language)), "--tag", f"language_runner:{language}"),
+        capture_output=True,
+        timeout=120,
+        text=True,
+        check=True,
+    )
+
+BUILT_LANGUAGES = get_built_docker_language_runners()
+
+# `tempdir` creates 'secure' temp folders.
+# These are not available to other users.
+# docker on mac runs as a different user.
+# None of the temp folders from env work because again - owned by the user
+#   environ.get('TMPDIR') or environ.get('TEMP') or environ.get('TMP') or
+tempdir = Path('.').joinpath('_language_runner')
+tempdir.mkdir(exist_ok=True)
 
 
-def pytest_ignore_collect(collection_path: Path, path, config: pytest.Config):
+class ProjectItemSpec(NamedTuple):
+    name: str
+    language: str
+    version: str
+    code: str
+
+    def exec_language(self, language_args: tuple[str]):
+        if self.language not in BUILT_LANGUAGES:
+            BUILT_LANGUAGES.add(self.language)
+            build_docker_language_runner(self.language)
+
+        workdir = f"/{self.language}"
+        docker_args = (
+            "docker", "run", "--rm",
+            "--workdir", workdir,
+            "--volume", f"{tempdir.absolute()}:{workdir}:rw",
+            f"language_runner:{self.language}",  # should match image name built from `build_docker_language_runner`
+        )
+        result = subprocess.run(
+            docker_args + language_args,
+            capture_output=True,
+            timeout=30,
+            text=True,
+            #check=True,
+        )
+        if result.returncode != 0:
+            raise ProjectCompileException(self, result)
+
+
+class ProjectCompileException(Exception):
+    def __init__(self, spec: ProjectItemSpec, result: subprocess.CompletedProcess):
+        super().__init__()
+        self.spec = spec
+        self.result = result
+
+
+def compile_test_python(spec: ProjectItemSpec):
+    path_code_file = tempdir.joinpath(spec.name)
+    path_code_file.write_text(spec.code)
+    spec.exec_language(("python3", "-m", "py_compile", path_code_file.name))
+
+
+def compile_test_java(spec: ProjectItemSpec):
+    # java? rename file?
+    pass
+
+
+def compile_test_csharp(spec: ProjectItemSpec):
+    # csharp create manifest?
+    pass
+
+
+LANGUAGES: MappingProxyType[str, Callable] = MappingProxyType(
+    {"py": compile_test_python}
+)
+
+
+# PyTest -----------------------------------------------------------------------
+
+def pytest_ignore_collect(collection_path: Path, config: pytest.Config):
     try:
         collection_path.relative_to(PATH_PROJECTS.absolute())
         return False
@@ -28,11 +121,15 @@ def pytest_collect_file(parent: pytest.Dir, file_path: Path):
         return ProjectFile.from_parent(parent, path=file_path)
 
 
-class ProjectItemSpec(TypedDict):
-    name: str
-    language: str
-    version: str
-    code: str
+#def pytest_collected():
+#    # TODO: this is not a real name I need the docs to look it up
+#    for language in {test.spec.language for test in collected.tests}:
+#        build_docker_language_runner(language)
+
+
+# TODO: correct hook for teardown
+#def pytest_teardown():
+#    tempdir.cleanup()
 
 
 class ProjectFile(pytest.File):
@@ -45,7 +142,9 @@ class ProjectFile(pytest.File):
                 yield ProjectItem.from_parent(
                     self,
                     name=name,
-                    spec=ProjectItemSpec(name=name, language=language, version=version, code=code),
+                    spec=ProjectItemSpec(
+                        name=name, language=language, version=version, code=code
+                    ),
                 )
 
 
@@ -55,35 +154,21 @@ class ProjectItem(pytest.Item):
         self.spec = spec
 
     def runtest(self):
-        if self.spec['language'] not in LANGUAGES_SUPPORTED:
-            raise pytest.skip.Exception(f"Unsupported language {self.spec['language']}")
+        if self.spec.language not in LANGUAGES.keys():
+            raise pytest.skip.Exception(f"Unsupported language {self.spec.language}")
+
+        LANGUAGES[self.spec.language](self.spec)
 
         # https://stackoverflow.com/questions/66037780/how-do-i-require-fixtures-in-a-pytest-plugin
         # No fixtures in plugins?
 
-        with tempfile.TemporaryDirectory() as tempdir:
-            dispatch_compile_test(tempdir, **self.spec)
-
-    def repr_failure(self, excinfo):
+    def repr_failure(self, exinfo: pytest.ExceptionInfo):
         """Called when self.runtest() raises an exception."""
-        if isinstance(excinfo.value, ProjectCompileException):
-            return "\n".join(
-                [
-                    "usecase execution failed",
-                    "   spec failed: {1!r}: {2!r}".format(*excinfo.value.args),
-                    "   no further details known at this point.",
-                ]
-            )
-        return super().repr_failure(excinfo)
+        if isinstance(exinfo.value, subprocess.CalledProcessError):
+            return exinfo.value.stderr
+        if isinstance(exinfo.value, ProjectCompileException):
+            return exinfo.value.result.stdout + exinfo.value.result.stderr + exinfo.value.spec.code
+        return super().repr_failure(exinfo)
 
     def reportinfo(self):
         return (self.path, 0, f"usecase: {self.name}")
-
-
-class ProjectCompileException(Exception):
-    """Custom exception for error reporting."""
-
-
-def dispatch_compile_test(tempdir, name, language, version, code):
-    breakpoint()
-    # raise ProjectCompileException(self, self.name, self.code)
