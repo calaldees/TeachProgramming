cc3544b015352bfe5c5899286b79334b4304f48a
Allan Callaghan <allan@calaldees.uk>
2025-09-03 09:37:17 +0100

    Progress with yaff/draw font loaders

--- teachprogramming/static/projects/graphics/font/font_loader.js
+++ teachprogramming/static/projects/graphics/font/font_loader.js
@@ -1,4 +1,4 @@
-import { range, zip } from './core.js'
+import { range, zip, fetch_text } from './core.js'
 import { Gfx } from './animation_base.js'
 
 // From Img --------------------------------------------------------------------
@@ -14,31 +14,50 @@ export function cut_font_chars_from_img(img, char_seq, w=8, h=8) {
     )
 }
 
-// From text (yaff,draw) -------------------------------------------------------
+// From text `yaff`  -----------------------------------------------------------
 
-function* extract_font_text_chars(text, foreground='@', background='.', width=8, height=8) {
-    text = text.replaceAll(/\s/sg, '')
-    const REGEX_CHAR = new RegExp(String.raw`[${background}${foreground}]{${width*height}}`, "isg")
-    for (let match of text.matchAll(REGEX_CHAR)) {yield match[0]}
-}
-function* extract_unicode_sequence(text) {
-    const REGEX_UNICODE = new RegExp(String.raw`u\+([0123456789abcdef]+):`, "ig")
-    for (let match of text.matchAll(REGEX_UNICODE)) {
-        yield String.fromCharCode(parseInt('0x'+match[1],16))
-    }
-}
 function text_char_to_ImageData(char, foreground='@', width=8, height=8) {
     const [WHITE_PIXEL, BLACK_PIXEL] = [[255,255,255,255],[0,0,0,0]]
     const pixel_data = [...char].map(c=>c==foreground?WHITE_PIXEL:BLACK_PIXEL).flat()
     return new ImageData(new Uint8ClampedArray(pixel_data), width, height)
 }
-export async function parse_font_unicode(data, char_seq=undefined) {
+function* _parse_yaff(text, foreground='@', background='.', width=8, height=8) {
+    const REGEX_UNICODE_LABEL_AND_CHAR = new RegExp(String.raw`u\+([0123456789abcdef]+):(.+?)\n\n`, "igs")
+    const REGEX_CHAR = new RegExp(String.raw`[${background}${foreground}]{${width*height}}`, "isg")
+    for (let match of text.matchAll(REGEX_UNICODE_LABEL_AND_CHAR)) {
+        let [_, unicode, text_char] = match
+        if (text_char=text_char.replaceAll(/\s/sg, '').match(REGEX_CHAR)[0]) {
+            yield [
+                String.fromCharCode(parseInt('0x'+unicode,16)),
+                text_char_to_ImageData(text_char,foreground,width,height),
+            ]
+        }
+    }
+}
+export async function parse_yaff(filename) {
+    const oo = Object.fromEntries(_parse_yaff(await fetch_text(filename)))
     return Object.fromEntries(zip(
-        char_seq || extract_unicode_sequence(data),
+        Object.keys(oo),
+        await Promise.all(Object.values(oo).map(i=>createImageBitmap(i))),
+    ))
+}
+
+// From text `draw`  -----------------------------------------------------------
+
+function* extract_font_text_chars(text, foreground='@', background='.', width=8, height=8) {
+    const REGEX_CHAR = new RegExp(String.raw`[${background}${foreground}]{${width*height}}`, "isg")
+    for (let match of text.replaceAll(/\s/sg, '').matchAll(REGEX_CHAR)) {yield match[0]}
+}
+export async function parse_draw(filename, char_seq) {
+    const text = await fetch_text(filename)
+    return Object.fromEntries(zip(
+        char_seq,
         await Promise.all(
-            [...extract_font_text_chars(data)]
-            .map(char=>text_char_to_ImageData(char))
-            .map(async (img_data)=>await createImageBitmap(img_data)),
+            [...extract_font_text_chars(text)]
+            .map(c=>text_char_to_ImageData(c,'#',8,8))
+            .map(i=>createImageBitmap(i)),
         )
     ))
 }
+
+export const SEQUENCE_AMSTRAD = String.raw`◻⎾⏊⏌⚡︎⊠✓⍾←→↓↑↡↲⊗⊙⊟◷◶◵◴⍻⎍⊣⧖⍿␦⊖◰◱◲◳!"#$%&’()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]↑_\`abcdefghijklmnopqrstuvwxyz{|}~  ▘▝▀▖▌▞▛▗▚▐▜▄▙▟█·╵╶└╷│┌├╴┘─┴┐┤┬┼^´¨£©¶§‘¼½¾±÷¬¿¡αβγδεθλμπσφψχωΣΩ🮠🮡🮣🮢🮧🮥🮦🮤🮨🮩🮮╳╱╲🮕▒▔▕▁▏◤◥◢◣🮎🮍🮏🮌🮜🮝🮞🮟☺☹♣♦♥♠○●□■♂♀♩♪☼🙭⭣⭠⭢▲▼▶◀🯆🯅𜱣🯈⭥⭤`
