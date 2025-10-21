a0963bde69693476c78dde08051320c4fc46e4af
Allan Callaghan <allan@calaldees.uk>
2025-10-21 08:12:25 +0100

    Pygame UDP wrapper looks good and typed Gotta think about sending game state as bytes now. This is going to need thought

--- teachprogramming/static/projects/game/game_udp.py
+++ teachprogramming/static/projects/game/game_udp.py
@@ -1,6 +1,6 @@
 # uv run --with pygame game_udp.py
 from collections.abc import Set
-from typing import NamedTuple
+from typing import Tuple, NamedTuple
 import socket
 import threading
 import multiprocessing
@@ -45,55 +45,51 @@ class Ship():
 
 
 class NetworkUDP():
+    class Addr(NamedTuple):
+        host: str
+        port: int
     class UDPMessage(NamedTuple):
         data: bytes = b''
-        addr_from: str = ''
+        addr_from: Tuple[str, int] = ('', 0)  # meant to be Addr, but cant
         def __bool__(self) -> bool: return bool(self.data)
-
     def __init__(self, addr_to='127.0.0.1', port=5005):
-        self.recv_queue = multiprocessing.Queue()
-        self.addr_to = (addr_to, port)
+        self.recv_queue: multiprocessing.Queue[NetworkUDP.UDPMessage] = multiprocessing.Queue()
+        self.addr_to = self.Addr(addr_to, port)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             self.sock.bind(('0.0.0.0', port))
         except OSError:
-            log.debug('cant bind socket - is another client is already running on this port?')
+            log.warning('cant bind socket - is another client is already running on this port?')
         thread = threading.Thread(target=self._recv_thread)
         thread.daemon=True
         thread.start()
-
     def _recv_thread(self) -> None:
         while True:
-            self.recv_queue.put_nowait(self.sock.recvfrom(1024))
-            #log.debug(f"received bytes: {data} from {self.addr_to}")
-            #try:
-            #except queue.ShutDown:  # TODO: may be unneeded
-            #    log.error('wha?')
-            #    break
-
-    def send(self, data: bytes) -> None:
-        self.sock.sendto(data, self.addr_to)
-
+            self.recv_queue.put_nowait(self.UDPMessage(*self.sock.recvfrom(1024)))
+    def send(self, data: bytes, addr_to: Addr | None = None) -> None:
+        self.sock.sendto(data, addr_to or self.addr_to)
     def get(self) -> UDPMessage:
-        try               : return self.UDPMessage(*self.recv_queue.get_nowait())
+        try               : return self.recv_queue.get_nowait()
         except queue.Empty: return self.UDPMessage()
+
 
 class UDPDemo(PygameBase):
     def __init__(self, network: NetworkUDP):
         self.network = network
+        self.ship = Ship()
         super().__init__(resolution=(640,360))
 
     def loop(self, s, frame):
         while msg := self.network.get():
-            (_x, _y) = map(int, msg.data.decode('utf8').split(','))
-            s.blit(DEFAULT_IMAGE, (_x, _y))
+            self.ship.x, self.ship.y = map(float, msg.data.decode('utf8').split(','))
+            #s.blit(DEFAULT_IMAGE, (_x, _y))
 
         mouse_x, mouse_y = pygame.mouse.get_pos()
         #s.blit(DEFAULT_IMAGE, (mouse_x, mouse_y))
-
         if frame%3 == 0:
             self.network.send(f'{mouse_x},{mouse_y}'.encode('utf8'))
 
+        s.blit(self.ship.image, self.ship.pos)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
