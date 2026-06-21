07c25aaaad1003d6b19c3965fbc78a2ac7e61384
Allan Callaghan <allan.callaghan@global.com>
2026-06-21 16:56:16 +0100

    I got the spring force acting in the right direction. Pretty weird trial and error maths. I probably need to sit down with a peice of paper.

--- teachprogramming/static/projects/game/physics.py
+++ teachprogramming/static/projects/game/physics.py
@@ -13,6 +13,8 @@ class Point():
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.x},{self.y})'
     @classmethod
     def from_array(cls, x:float, y:float) -> Self:
         return cls(x, y)
@@ -31,6 +33,8 @@ class Line():
     def __init__(self, p1: Point, p2: Point):
         self.p1 = p1
         self.p2 = p2
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.p1},{self.p2})'
     @classmethod
     def from_array(cls, x1:float, y1:float, x2:float, y2:float) -> Self:
         return cls(Point(x1, y1), Point(x2, y2))
@@ -41,20 +45,27 @@ class Line():
         self.p1.translate(p)
         self.p2.translate(p)
     @staticmethod
-    def _gradient(p1:Point, p2:Point) -> float:
+    def _dx_dy(p1:Point, p2:Point) -> float:
         if (p1.y==p2.y):
             return math.inf
         return (p1.x-p2.x)/(p1.y-p2.y)
     @property
-    def gradient(self) -> float:
-        return self._gradient(self.p1, self.p2)
+    def dx_dy(self) -> float:
+        return self._dx_dy(self.p1, self.p2)
     @property
     def length(self) -> float:
         return self.p1.distance(self.p2)
     @property
     def angle(self) -> float:
-        # atan is wrong - we get inf ... this should be a smooth 0to1 radians
-        return math.atan(self.gradient)
+        dx_dy = self.dx_dy
+        if dx_dy==math.inf and self.p1.x > self.p2.x:
+            # This is a weird special case I don't understand
+            return math.pi/2
+        angle = math.atan(dx_dy)
+        if self.p1.y <= self.p2.y:
+            angle += math.pi
+        return angle
+        
     def intersect(self, line) -> Point | bool:
         """
         https://paulbourke.net/geometry/pointlineplane/javascript.txt
@@ -80,6 +91,11 @@ class Vector():
     def __init__(self, x:float=0, y:float=0):
         self.x = x
         self.y = y
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.x:.2f},{self.y:.2f})'
+    @property
+    def to_array(self) -> Sequence[float]:
+        return (self.x, self.y)
     def add(self, b: Self) -> None:
         self.x += b.x
         self.y += b.y
@@ -123,13 +139,17 @@ class Spring():
         self.mat = mat
         self.line = Line(a.p, b.p)
         self.initial_length = self.line.length
-    def apply_force(self) -> None:
+    def add_force(self) -> None:
         length = self.line.length
         length_factor = (length-self.initial_length)/self.initial_length
-        force_magnitude = (self.mat.tension if length > self.initial_length else self.mat.compression) * length_factor
-        force_vector = Vector.from_angle(self.line.angle, force_magnitude)
-        self.a.add_force(force_vector)
-        self.b.add_force(-force_vector)
+        force_mat = (self.mat.tension if length > self.initial_length else self.mat.compression)
+        force_magnitude = length_factor * force_mat
+        # positive = line is longer = apply force to attract masses
+        # negative = line is shorter = apply force to repl masses
+        angle = self.line.angle
+        force_vector = Vector.from_angle(angle, force_magnitude)
+        self.a.add_force(-force_vector)
+        self.b.add_force(force_vector)
 
 
 class Lattice(NamedTuple):
@@ -171,19 +191,44 @@ class Lattice(NamedTuple):
 class GameDemo(PygameBase):
     def __init__(self):
         self.l = Lattice.build(100,100,3,3,10,1,SpringMaterial(1,1))
+        # m1 = Mass(Point(100,100), 10)
+        # m2 = Mass(Point(110,100), 10)
+        # self.l = Lattice(
+        #    masses=(m1,m2,),
+        #    springs=(Spring(m1,m2,SpringMaterial(1,1)),)
+        # )
+        # m2.p.x += 2
+        # m2.p.y += 2
         super().__init__()
 
     def loop(self, screen, frame):
         s = screen
         width, height = s.get_size()
 
+        # m2 =self.l.masses[1]
+        # m2.p.x = 100 + math.sin(frame/60)*12
+        # m2.p.y = 100 + math.cos(frame/60)*12
+        # if self.keys[pygame.K_UP]:
+        #     m2.p.y += -1
+        # if self.keys[pygame.K_DOWN]:
+        #     m2.p.y += 1
+        # if self.keys[pygame.K_RIGHT]:
+        #     m2.p.x += 1
+        # if self.keys[pygame.K_LEFT]:
+        #     m2.p.x += -1
+
         for mass in self.l.masses:
             mass.add_force(Vector(0,mass.mass*0.01))  # Gravity
             #mass.add_force(Vector(-mass.vel.x/300,-mass.vel.y/300))  # Air Viscosity
-        for mass in self.l.masses:
-            mass.apply_force()  # move all the masses with force - remove the accumulated force
+            mass.vel.x *= 0.99
+            mass.vel.y *= 0.99
         for spring in self.l.springs:
-            spring.apply_force()
+            spring.add_force()
+        c = pygame.Color("green")
+        for mass in self.l.masses:
+            pygame.draw.line(s, c, mass.p.to_array , (mass.p.x+(mass.force.x*20), mass.p.y+(mass.force.y*20)), 1)
+            mass.apply_force()  # move all the masses with force - remove the accumulated force
+            #mass.force.reset()
         for mass in self.l.masses:
             if mass.p.y > height:
                 mass.p.y = float(height)
