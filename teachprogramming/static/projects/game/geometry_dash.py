b9b0a661b8adc244f0d11986af1ba77f85589b12
Allan Callaghan <allan@calaldees.uk>
2026-09-07 21:37:35 -0400

    Attempt at rotating and colision detection

--- teachprogramming/static/projects/game/geometry_dash.py
+++ teachprogramming/static/projects/game/geometry_dash.py
@@ -6,7 +6,11 @@ import pygame
 from animation_base_pygame import PygameBase
 
 
-def load_geometry_dash_levels(path: Path, width=120) -> Sequence[str]:
+type LevelData = Sequence[str]
+type LevelDataSlice = Sequence[str]
+
+
+def load_geometry_dash_levels(path: Path, width=120) -> LevelData:
     lines = []
     with path.open() as f:
         while line := f.readline():
@@ -31,22 +35,32 @@ class GeometryDash(PygameBase):
         self.tiles = load_tiles(Path('geometry_dash.png'), '@^#_', self.tile_size)
 
         self.level: int = 0
-        self.speed: float = 1.21
+        self.speed: float = 1.71
 
         self.x: float = 0.0
 
         self.y = 100
         self.y_vel = 0
         self.jump_vel = -10
-        super().__init__()
+        super().__init__(fps=15)
 
-    def data_slice_screen(self, level:int, x:float, lines_per_level:int=8):
-        x_start:int = int(x) // self.tile_size
-        x_end  :int = x_start + (self.width // self.tile_size) + self.tile_size
+    def data_slice_screen(self, level:int, x:float, lines_per_level:int=8) -> LevelDataSlice:
+        level_width_tiles = len(self.level_data[0])
+        screen_width_tiles = (self.width // self.tile_size)
+        x_start:int = max(0, int(x) // self.tile_size)
+        x_end  :int = min(level_width_tiles, x_start + screen_width_tiles)
         return [
             self.level_data[line_num][x_start:x_end]
             for line_num in range((level)*lines_per_level, (level+1)*lines_per_level)
         ]
+
+    def draw_level(self, screen: pygame.screen, screen_data: LevelDataSlice, x_offset: int) -> None:
+        #tile_x_offset = int(self.x % self.tile_size)
+        for tile_y, line in enumerate(screen_data):
+            for tile_x, chr in enumerate(line):
+                if chr == ' ': continue
+                screen.blit(self.tiles[chr], (tile_x * self.tile_size + x_offset, tile_y * self.tile_size))
+
 
     def loop(self, screen, frame):
         s = screen
@@ -58,8 +72,14 @@ class GeometryDash(PygameBase):
         #    self.x += -1
 
         x_draw_offset = (self.width / self.tile_size) * self.speed * 4
-        screen_data = self.data_slice_screen(self.level, self.x-x_draw_offset)
+        screen_data = self.data_slice_screen(self.level, self.x)
 
+        self.y += self.y_vel
+        x_tile = int(self.x)//self.tile_size
+        y_tile = int(self.y)//self.tile_size
+        chr = screen_data[y_tile][x_tile]
+        if chr != ' ':
+            pygame.draw.rect(s, pygame.Color("#f0b000"), (self.x, self.y, self.tile_size, self.tile_size))
         is_on_ground = self.y >= self.height - self.tile_size
         self.y = min(self.height - self.tile_size, self.y)
         if is_on_ground:
@@ -68,14 +88,19 @@ class GeometryDash(PygameBase):
                 self.y_vel = self.jump_vel
         else:
             self.y_vel += 1
-        self.y += self.y_vel
 
-        tile_x_offset = int(self.x % self.tile_size)
-        for tile_y, line in enumerate(screen_data):
-            for tile_x, chr in enumerate(line):
-                if chr == ' ': continue
-                s.blit(self.tiles[chr], (tile_x * self.tile_size - tile_x_offset, tile_y * self.tile_size))
-        s.blit(self.tiles['@'], (x_draw_offset, self.y))
+        x_offset = self.tile_size-int(self.x) % self.tile_size
+        print(x_offset)
+        self.draw_level(screen, screen_data, x_offset)
+
+        rotated_image = pygame.transform.rotate(self.tiles['@'], self.y*8)
+        rotated_rect = rotated_image.get_rect()
+        rotated_rect.center = (self.tile_size//2, self.tile_size//2)
+        rotated_rect.x = x_draw_offset
+        rotated_rect.y = self.y
+        s.blit(rotated_image, rotated_rect)
+        #s.blit(self.tiles['@'], (x_draw_offset, self.y))
+
 
 
 if __name__ == '__main__':
