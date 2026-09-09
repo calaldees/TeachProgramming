013cbde8c9b1ef05b0081895537b847971593cd6
Allan Callaghan <allan@calaldees.uk>
2026-09-09 11:16:11 -0400

    Tidy (slowly) of geometry dash

--- teachprogramming/static/projects/game/geometry_dash.py
+++ teachprogramming/static/projects/game/geometry_dash.py
@@ -30,7 +30,7 @@ def load_tiles(path: Path, seq: str, w: int, h: int = 0) -> Tiles:
     }
 
 
-def draw_tiles(screen: pygame.Surface, tiles: Tiles, screen_data: LevelData, x:int, y:int) -> None:
+def draw_tiles(screen: pygame.Surface, tiles: Tiles, screen_data: LevelData, x: int, y: int) -> None:
     t_reference = next(iter(tiles.values()))  # get first tile and extract the tile dimensions
     tw, th = (t_reference.width, t_reference.height)
     stw, sth = (screen.width//tw, screen.height//th)
@@ -63,9 +63,10 @@ class GeometryDash(PygameBase):
 
         self.level: int = 0
 
-        self.speed: float = 2.5
+        self.screen_speed: float = 2.5
         self.jump_vel: float = -5
         self.jump_vel_inc: float = 0.2
+        self.rotate_inc: float = -4
         self.reset()
 
         super().__init__(fps=60)
@@ -76,61 +77,36 @@ class GeometryDash(PygameBase):
         self.y_vel:float = 0
         self.rotation:float = 0
 
-    def data_slice_screen(self, level:int, x:float, lines_per_level:int=8) -> LevelDataSlice:
-        level_width_tiles = len(self.level_data[0])
-        screen_width_tiles = (self.width // self.tile_size)
-        x_start:int = max(0, int(x) // self.tile_size)
-        x_end  :int = min(level_width_tiles, x_start + screen_width_tiles)
-        return [
-            self.level_data[line_num][x_start:x_end]
-            for line_num in range((level)*lines_per_level, (level+1)*lines_per_level)
-        ]
-
-    def draw_level(self, screen: pygame.screen, screen_data: LevelDataSlice, x_scroll_backshift: int) -> None:
-        #tile_x_offset = int(self.x % self.tile_size)
-        for tile_y, line in enumerate(screen_data):
-            for tile_x, chr in enumerate(line):
-                if chr == ' ': continue
-                screen.blit(self.tiles[chr], (tile_x * self.tile_size + x_scroll_backshift, tile_y * self.tile_size))
-
     def loop(self, screen, frame):
         s = screen
 
-        y_floor = self.height - self.tile_size
-        x_screen_offset = 50 # (self.width / self.tile_size) * self.speed * 4
+        y_floor_max = self.height - self.tile_size
+        x_screen_offset = (s.width // self.tile_size) * self.screen_speed
 
-        self.x += self.speed
+        self.x += self.screen_speed
         self.y += self.y_vel
-        #if self.keys[pygame.K_RIGHT]:
-        #    self.x += 1
-        #if self.keys[pygame.K_LEFT]:
-        #    self.x += -1
-        screen_data = self.data_slice_screen(self.level, self.x)
 
-        x_tile = int(x_screen_offset)//self.tile_size
+        x_tile = int(self.x+x_screen_offset)//self.tile_size
         y_tile = int(self.y)//self.tile_size
-        if screen_data[y_tile+1][x_tile] not in (' ', '^'):  # Title below
+        if self.level_data[y_tile+1][x_tile] not in (' ', '^'):  # Title below
             pygame.draw.rect(s, pygame.Color("#f0b000"), (x_screen_offset, self.y, self.tile_size, self.tile_size))
-            y_floor = y_tile * self.tile_size
-        if screen_data[y_tile][x_tile] not in (' ', '_'):
+            y_floor_max = y_tile * self.tile_size
+        if self.level_data[y_tile][x_tile] not in (' ', '_'):  # Tile above?
             self.y_vel = 0
-            #self.y = self.tile_size * y_tile+1
-            y_tile += 1
-        if screen_data[y_tile][x_tile+1] not in (' ', '_'):  # tile_infront
+            y_tile += 1  # HACK: for future collisions, treat this as the tile below
+        if self.level_data[y_tile][x_tile+1] not in (' ', '_'):  # tile_infront
             self.reset()
-        is_on_ground = (self.y >= y_floor)
-        self.y = min(y_floor, self.y)
 
+        is_on_ground = (self.y >= y_floor_max)
+        self.y = min(y_floor_max, self.y)
         if is_on_ground:
             self.y_vel = 0
             if self.keys[pygame.K_SPACE]:
                 self.y_vel = self.jump_vel
         else:
             self.y_vel += self.jump_vel_inc
-            self.rotation -= 4
+            self.rotation += self.rotate_inc
 
-        x_scroll_backshift = self.tile_size - int(self.x) % self.tile_size
-        #self.draw_level(screen, screen_data, x_scroll_backshift)
         draw_tiles(screen, self.tiles, self.level_data, int(self.x), 0)
 
         #s.blit(self.tiles['@'], (x_draw_offset, self.y))
