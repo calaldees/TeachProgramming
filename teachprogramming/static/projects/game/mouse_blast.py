8fd0bdedf7bc2e3e1c194cacb81cf668a7f0f92e
Allan Callaghan <allan@calaldees.uk>
2026-09-10 23:03:37 -0400

    mouse_blast tidy with class to handle tiles

--- teachprogramming/static/projects/game/mouse_blast.py
+++ teachprogramming/static/projects/game/mouse_blast.py
@@ -7,50 +7,58 @@ from animation_base_pygame import PygameBase
 
 
 type LevelData = Sequence[str]
-type LevelDataSlice = Sequence[str]
 type Tile = str
 type Tiles = Mapping[str, pygame.Surface]
 
 
-def load_levels(path: Path, width=120) -> LevelData:
-    lines = []
-    with path.open() as f:
-        while line := f.readline():
-            lines.append(line.removesuffix('\n').ljust(width, ' '))
-    return lines
+class TileManager():
 
+    @staticmethod
+    def load_levels(path: Path, width=120) -> LevelData:
+        lines = []
+        with path.open() as f:
+            while line := f.readline():
+                lines.append(line.removesuffix('\n').ljust(width, ' '))
+        return lines
 
-def load_tiles(path: Path, seq: str, w: int, h: int = 0) -> Tiles:
-    h = h or w
-    img = pygame.image.load(path)
-    ww, hh = img.get_size()
-    return {
-        seq[i]: img.subsurface(((i*w)%ww, ((i*w)//ww)*h, w, h))
-        for i in range(min((ww//w)*(hh//h), len(seq)))
-    }
+    @staticmethod
+    def load_tiles(path: Path, seq: str, w: int, h: int = 0) -> Tiles:
+        h = h or w
+        img = pygame.image.load(path)
+        ww, hh = img.get_size()
+        return {
+            seq[i]: img.subsurface(((i*w)%ww, ((i*w)//ww)*h, w, h))
+            for i in range(min((ww//w)*(hh//h), len(seq)))
+        }
 
+    def __init__(self, tiles: Tiles, level_data: LevelData):
+        self.tiles = tiles
+        self.level_data = level_data
 
-def draw_tiles(screen: pygame.Surface, tiles: Tiles, screen_data: LevelData, screen_x: int, screen_y: int) -> None:
-    max_x, max_y = (len(screen_data[0]), len(screen_data))
-    t_reference = next(iter(tiles.values()))  # get first tile and extract the tile dimensions
-    tw, th = (t_reference.width, t_reference.height)
-    stw, sth = (screen.width//tw, screen.height//th)
-    tx, ty = (screen_x//tw, screen_y//th)
-    x_offset, y_offset = (screen_x%tw, screen_y%th)
-    for _y in range(sth+2):
-        for _x in range(stw+2):
-            x, y = (_x+tx, _y+ty)
-            if x<0 or y<0 or x>=max_x or y>=max_y: continue
-            t = screen_data[y][x]
-            if t == ' ': continue
-            screen.blit(tiles[t], ((_x*tw)-x_offset, (_y*th)-y_offset))
+        self.level_width, self.level_height = (len(level_data[0]), len(level_data))
+        _t = next(iter(tiles.values()))  # get first tile and extract the tile dimensions
+        self.tile_width, self.tile_height = (_t.width, _t.height)
+
+    def draw_tiles(self, screen: pygame.Surface, screen_x: int, screen_y: int) -> None:
+        tw, th = (self.tile_width, self.tile_height)
+        stw, sth = (screen.width//tw, screen.height//th)
+        tx, ty = (screen_x//tw, screen_y//th)
+        x_offset, y_offset = (screen_x%tw, screen_y%th)
+        for _y in range(sth+2):
+            for _x in range(stw+2):
+                x, y = (_x+tx, _y+ty)
+                if x<0 or y<0 or x>=self.level_width or y>=self.level_height: continue
+                t = self.level_data[y][x]
+                if t == ' ': continue
+                screen.blit(self.tiles[t], ((_x*tw)-x_offset, (_y*th)-y_offset))
 
 
 class MouseBlast(PygameBase):
     def __init__(self):
-        self.level_data = load_levels(Path('geometry_dash.txt'))
-        self.tile_size = 32
-        self.tiles = load_tiles(Path('geometry_dash.png'), '@^#_', self.tile_size)
+        self.tiles = TileManager(
+            tiles=TileManager.load_tiles(Path('geometry_dash.png'), '@^#_', 32),
+            level_data=TileManager.load_levels(Path('geometry_dash.txt')),
+        )
         super().__init__(fps=60)
         self.reset()
 
@@ -66,15 +74,18 @@ class MouseBlast(PygameBase):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         fire = self.keys[pygame.K_SPACE]
 
-        screen_x = self.x-(screen.width //2) + (mouse_x-(screen.width //2))
-        screen_y = self.y-(screen.height//2) + (mouse_y-(screen.height//2))
-        draw_tiles(screen, self.tiles, self.level_data, screen_x, screen_y)
+        screen_x = self.x - (screen.width //2) + (mouse_x-(screen.width //2))
+        screen_y = self.y - (screen.height//2) + (mouse_y-(screen.height//2))
+        screen_x = min(max(0, screen_x), self.tiles.level_width*self.tiles.tile_width)
+        screen_y = min(max(0, screen_y), self.tiles.level_height*self.tiles.tile_height)
+        self.tiles.draw_tiles(screen, screen_x, screen_y)
 
         player_x, player_y = (self.x-screen_x, self.y-screen_y)
         pygame.draw.rect(screen, pygame.Color("#f0b000"), (player_x, player_y, 5, 5))
         pygame.draw.rect(screen, pygame.Color("#f00000"), (mouse_x, mouse_y, 5, 5))
         if fire:
             pygame.draw.line(screen, pygame.Color("#00ffff"), (player_x, player_y), (mouse_x, mouse_y), 1)
+
 
 if __name__ == '__main__':
     MouseBlast().run()
