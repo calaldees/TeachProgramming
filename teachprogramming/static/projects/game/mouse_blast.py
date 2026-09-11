e19849ae70e584002328a2e022455b81feada423
Allan Callaghan <allan@calaldees.uk>
2026-09-11 00:05:54 -0400

    mouse_blast now has bullets

--- teachprogramming/static/projects/game/mouse_blast.py
+++ teachprogramming/static/projects/game/mouse_blast.py
@@ -1,4 +1,5 @@
-from collections.abc import Mapping, Sequence
+import math
+from collections.abc import Mapping, Sequence, MutableSequence
 from pathlib import Path
 
 import pygame
@@ -6,8 +7,9 @@ import pygame
 from animation_base_pygame import PygameBase
 
 
-type LevelData = Sequence[str]
-type Tile = str
+type TileID = str  # str of length 1
+type LevelRow = str
+type LevelData = Sequence[LevelRow]
 type Tiles = Mapping[str, pygame.Surface]
 
 
@@ -48,9 +50,36 @@ class TileManager():
             for _x in range(stw+2):
                 x, y = (_x+tx, _y+ty)
                 if x<0 or y<0 or x>=self.level_width or y>=self.level_height: continue
-                t = self.level_data[y][x]
+                t:TileID = self.level_data[y][x]
                 if t == ' ': continue
                 screen.blit(self.tiles[t], ((_x*tw)-x_offset, (_y*th)-y_offset))
+
+    def get_tile(self, x:int, y:int) -> TileID:
+        return self.level_data[y//self.tile_height][x//self.tile_width]
+
+
+class Bullet():
+    def __init__(self, x:float, y:float, x_vel:float, y_vel:float):
+        self.x = x
+        self.y = y
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.duration:int = 0
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+        self.duration += 1
+
+
+def angle(p1_x, p1_y, p2_x, p2_y) -> float:
+    dx_dy = (p1_x-p2_x) / (p1_y-p2_y) if p1_y!=p2_y else math.inf
+    if dx_dy==math.inf and p1_x > p2_x:
+        return math.pi/2  # This is a weird special case I don't understand
+    angle = math.atan(dx_dy)
+    if p1_y <= p2_y:
+        angle += math.pi
+    return angle
 
 
 class MouseBlast(PygameBase):
@@ -65,6 +94,7 @@ class MouseBlast(PygameBase):
     def reset(self) -> None:
         self.x: float = self.screen.width
         self.y: float = self.screen.height
+        self.bullets: MutableSequence[Bullet] = []
 
     def loop(self, screen, frame):
         if self.keys[pygame.K_w]: self.y-=1
@@ -78,13 +108,28 @@ class MouseBlast(PygameBase):
         screen_y = self.y - (screen.height//2) + (mouse_y-(screen.height//2))
         screen_x = min(max(0, screen_x), self.tiles.level_width*self.tiles.tile_width)
         screen_y = min(max(0, screen_y), self.tiles.level_height*self.tiles.tile_height)
-        self.tiles.draw_tiles(screen, screen_x, screen_y)
-
         player_x, player_y = (self.x-screen_x, self.y-screen_y)
+
+        a = angle(mouse_x, mouse_y, player_x, player_y)
+        vel = 2.5
+        if fire:
+            self.bullets.append(Bullet(self.x, self.y, x_vel=math.sin(a)*vel, y_vel=math.cos(a)*vel))
+        for b in self.bullets:
+            b.move()
+        self.bullets = [
+            b
+            for b in self.bullets
+            if b.duration<200 and self.tiles.get_tile(int(b.x), int(b.y)) == ' '
+        ]
+
+        self.tiles.draw_tiles(screen, screen_x, screen_y)
         pygame.draw.rect(screen, pygame.Color("#f0b000"), (player_x, player_y, 5, 5))
         pygame.draw.rect(screen, pygame.Color("#f00000"), (mouse_x, mouse_y, 5, 5))
         if fire:
             pygame.draw.line(screen, pygame.Color("#00ffff"), (player_x, player_y), (mouse_x, mouse_y), 1)
+
+        for b in self.bullets:
+            pygame.draw.rect(screen, pygame.Color("#ffffff"), (b.x-screen_x, b.y-screen_y, 1, 1))
 
 
 if __name__ == '__main__':
